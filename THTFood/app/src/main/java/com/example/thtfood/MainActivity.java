d398e18e11160cb962d9e6ef984f84e353d99b3c
Tiến Đỗ <1951052198tien@ou.edu.vn>
2023-06-25 09:35:44 +0700

    Create srouce + design main_activity

---
+++ THTFood/app/src/main/java/com/example/thtfood/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.thtfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openLoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+}
+
