6de56fd536dd5f0f1dc853a83053abaf56762965
Tiến Đỗ <tiendo@bravesoft.com.vn>
2023-06-30 13:37:31 +0700

    Update home_activity

---
+++ THTFood/app/src/main/java/com/example/thtfood/HomeActivity.java
@@ -0,0 +1,50 @@
+package com.example.thtfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.thtfood.databinding.ActivityHomeBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        setDefaultMenuItem();
+
+        binding.botomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.home)
+                replaceFragment(new HomeFragment());
+            else if ( item.getItemId() == R.id.search) {
+                replaceFragment(new SearchFragment());
+            }
+            else if (item.getItemId() == R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
+    }
+    private void setDefaultMenuItem() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.botomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
