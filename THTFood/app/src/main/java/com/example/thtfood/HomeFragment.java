89a760dc100b4bd94829f5d7801509d8ec70416f
giatruong2511 <giatruong251101@gmail.com>
2023-07-18 21:49:27 +0700

    update_homefragmnet

--- THTFood/app/src/main/java/com/example/thtfood/HomeFragment.java
+++ THTFood/app/src/main/java/com/example/thtfood/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +20,7 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    LinearLayout horizontalLayout;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,7 +60,36 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Tạo View cho Fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Lấy đối tượng LinearLayout từ View
+        LinearLayout horizontalLayout = view.findViewById(R.id.horizontal_layout);
+        LinearLayout verticalLayout = view.findViewById(R.id.vertical_layout);
+        // Thêm các card vào horizontalLayout
+        LayoutInflater cardInflater = LayoutInflater.from(getActivity());
+        for (int i = 0; i < 5; i++) {
+            View cardView = cardInflater.inflate(R.layout.reshor_card, horizontalLayout, false);
+            // Thực hiện việc thiết lập dữ liệu cho cardView tại đây (như ảnh, tiêu đề, nội dung, v.v.)
+//            ImageView imageView = cardView.findViewById(R.id.imageView);
+//            TextView titleTextView = cardView.findViewById(R.id.titleTextView);
+//
+////            Thiết lập dữ liệu cho imageView và titleTextView
+//            imageView.setImageResource(R.drawable.your_image);
+//            titleTextView.setText("Your Title");
+            horizontalLayout.addView(cardView);
+        }
+        LayoutInflater cardInflater2 = LayoutInflater.from(getActivity());
+        for (int i = 0; i < 6; i++) {
+            View cardView = cardInflater2.inflate(R.layout.resver_card, verticalLayout, false);
+            // Thực hiện việc thiết lập dữ liệu cho cardView tại đây (như ảnh, tiêu đề, nội dung, v.v.)
+            verticalLayout.addView(cardView);
+        }
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
+
+
 }
