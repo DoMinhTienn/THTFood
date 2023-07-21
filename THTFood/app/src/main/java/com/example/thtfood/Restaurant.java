a0dc05434627979e1b93c7ab4d77a45e8c2ffdf9
Tiến Đỗ <1951052198tien@ou.edu.vn>
2023-07-22 00:18:51 +0700

    Update profile fragment and create class restaurant

--- THTFood/app/src/main/java/com/example/thtfood/Restaurant.java
+++ THTFood/app/src/main/java/com/example/thtfood/Restaurant.java
@@ -1,2 +1,40 @@
-package com.example.thtfood;public class Restaurant {
+package com.example.thtfood;
+
+public class Restaurant {
+    private String name;
+    private String avatar_path;
+
+    private String address;
+
+    public Restaurant(String name, String avatar_path, String address) {
+        this.name = name;
+        this.avatar_path = avatar_path;
+        this.address = address;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar_path() {
+        return avatar_path;
+    }
+
+    public void setAvatar_path(String avatar_path) {
+        this.avatar_path = avatar_path;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
