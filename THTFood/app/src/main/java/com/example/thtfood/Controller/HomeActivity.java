29ee1a58e7fd42416486d10b159539e0ecbade93
Tiến Đỗ <tiendo@bravesoft.com.vn>
2023-07-25 16:59:49 +0700

    test

--- THTFood/app/src/main/java/com/example/thtfood/Controller/HomeActivity.java
+++ THTFood/app/src/main/java/com/example/thtfood/Controller/HomeActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.thtfood.Model.User;
+import com.example.thtfood.Model.UserManager;
 import com.example.thtfood.R;
 import com.example.thtfood.databinding.ActivityHomeBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,18 +18,31 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class HomeActivity extends AppCompatActivity {
 
     ActivityHomeBinding binding;
+    User user = UserManager.getInstance().getUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new HomeFragment());
+        if(user != null && "1".equals(user.getRole()) ){
+            replaceFragment(new RestaurantManagerFragment());
+        }
+        else {
+            replaceFragment(new HomeFragment());
+        }
         setDefaultMenuItem();
 
         binding.botomNavigationView.setOnItemSelectedListener(item -> {
-            if(item.getItemId() == R.id.home)
-                replaceFragment(new HomeFragment());
+
+            if(item.getItemId() == R.id.home){
+                if(user != null && "1".equals(user.getRole()) ){
+                    replaceFragment(new RestaurantManagerFragment());
+                }
+                else {
+                    replaceFragment(new HomeFragment());
+                }
+            }
             else if ( item.getItemId() == R.id.search) {
                 replaceFragment(new SearchFragment());
             }
