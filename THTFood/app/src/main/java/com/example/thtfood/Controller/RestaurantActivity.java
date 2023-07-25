930387195eee5182c94d02006c3ba2c38cfbf4d6
giatruong2511 <giatruong251101@gmail.com>
2023-07-25 23:07:12 +0700

    update Restaurant_detail2

--- THTFood/app/src/main/java/com/example/thtfood/Controller/RestaurantActivity.java
+++ THTFood/app/src/main/java/com/example/thtfood/Controller/RestaurantActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.thtfood.Controller.ProductAdapter;
 import com.example.thtfood.Model.Product;
 import com.example.thtfood.R;
@@ -17,28 +22,56 @@ import java.util.List;
 public class RestaurantActivity extends AppCompatActivity {
     private RecyclerView recyclerViewProducts;
     private ProductAdapter productAdapter;
-    private ImageView imageView;
+
+    private TextView nameTextView;
+    private TextView addressTextView;
+    private ImageView imageRestarant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
-        imageView.setImageResource(R.drawable.bread);
-        recyclerViewProducts = findViewById(R.id.recyclerViewProducts);
+        imageRestarant = findViewById(R.id.imageRestarant);
+        nameTextView = findViewById(R.id.nameRes);
+        addressTextView = findViewById(R.id.adressRes);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String name = intent.getStringExtra("name");
+            String address = intent.getStringExtra("address");
+            String avatar = intent.getStringExtra("avatar");
+            nameTextView.setText(name);
+            addressTextView.setText(address);
+            Glide.with(this).load(avatar).into(imageRestarant);
+        }
+
         recyclerViewProducts = findViewById(R.id.recyclerViewProducts);
         recyclerViewProducts.setLayoutManager(new GridLayoutManager(this, 2)); // Số cột là 2
 
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product("Product 1", 10.99));
-        productList.add(new Product("Product 2", 15.49));
-        productList.add(new Product("Product 3", 15.49));
-        productList.add(new Product("Product 4", 15.49));
-        productList.add(new Product("Product 5", 15.49));
-        productList.add(new Product("Product 6", 15.49));
+        productList.add(new Product("Cơm sườn nướng", 30000.0));
+        productList.add(new Product("Cháo lòng ngon", 50000.0));
+        productList.add(new Product("Cháo lòng ngon", 50000.0));
+        productList.add(new Product("Cháo lòng ngon", 50000.0));
+        productList.add(new Product("Cháo lòng ngon", 50000.0));
+        productList.add(new Product("Cháo lòng ngon", 50000.0));
         // Thêm các sản phẩm khác vào danh sách nếu cần thiết
 
         productAdapter = new ProductAdapter(productList);
         recyclerViewProducts.setAdapter(productAdapter);
+
+        productAdapter.setOnProductClickListener(new ProductAdapter.OnProductClickListener() {
+            @Override
+            public void onProductClick(Product product) {
+                // Xử lý khi sản phẩm được click, chẳng hạn chuyển sang màn hình chi tiết sản phẩm và truyền dữ liệu sản phẩm
+                // qua Intent hoặc Bundle.
+                Intent intent = new Intent(RestaurantActivity.this, ProductDetailActivity.class);
+                intent.putExtra("product_name", product.getName());
+                intent.putExtra("product_price", product.getPrice());
+                startActivity(intent);
+            }
+        });
+
     }
 }
