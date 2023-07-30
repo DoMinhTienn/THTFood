42b562646baac2e8de89a65c14004e5f52eb2128
Tiến Đỗ <1951052198tien@ou.edu.vn>
2023-07-31 01:35:10 +0700

    Update menu restaurant

--- THTFood/app/src/main/java/com/example/thtfood/Controller/MenuRestaurantActivity.java
+++ THTFood/app/src/main/java/com/example/thtfood/Controller/MenuRestaurantActivity.java
@@ -1,36 +1,129 @@
 package com.example.thtfood.Controller;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.thtfood.Model.Menu;
 import com.example.thtfood.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MenuRestaurantActivity extends AppCompatActivity {
+    private static final int REQUEST_ADD_MENU = 1;
     private List<Menu> menuItems = new ArrayList<>();
     private RecyclerView recyclerView;
     private MenuAdapter menuAdapter;
+    private Button btnConfirm;
+    private  TextView tvNoData;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_restaurant);
-        bindingData();
         recyclerView = findViewById(R.id.recyclerViewMenu);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         menuAdapter = new MenuAdapter(menuItems);
         recyclerView.setAdapter(menuAdapter);
-
+        btnConfirm = findViewById(R.id.btnConfirm);
+        tvNoData = findViewById(R.id.tvNoData);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddMenuActivity();
+            }
+        });
+        loadData();
     }
 
-    private void bindingData(){
-        menuItems.add(new Menu("Món 1", "",100000, true));
-        menuItems.add(new Menu("Món 2", "",50000, true));
-        menuItems.add(new Menu("Món 3", "",80000, true));
+    private void loadData(){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        String restaurantId = currentUser.getUid();
+        DatabaseReference menuRef = FirebaseDatabase.getInstance().getReference()
+                .child("restaurants")
+                .child(restaurantId)
+                .child("menu");
+        menuItems.clear();
+        menuRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    recyclerView.setVisibility(View.VISIBLE);
+                    tvNoData.setVisibility(View.GONE);
+                    menuRef.addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            Menu menu = snapshot.getValue(Menu.class);
+                            menuItems.add(menu);
+                            menuAdapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+                else{
+                    if (menuItems.size() == 0) {
+                        recyclerView.setVisibility(View.GONE);
+                        tvNoData.setVisibility(View.VISIBLE);
+                    } else{
+                        recyclerView.setVisibility(View.VISIBLE);
+                        tvNoData.setVisibility(View.GONE);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+    private void openAddMenuActivity() {
+        Intent intent = new Intent(MenuRestaurantActivity.this, AddMenuActivity.class);
+        startActivityForResult(intent, REQUEST_ADD_MENU);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ADD_MENU && resultCode == RESULT_OK) {
+            loadData();
+        }
     }
 }
