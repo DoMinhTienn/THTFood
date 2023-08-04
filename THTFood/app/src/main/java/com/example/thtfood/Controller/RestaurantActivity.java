ccab90ab64ae53ff9f5895be4a26f2e4f33bd7b9
giatruong2511 <giatruong251101@gmail.com>
2023-08-04 22:17:57 +0700

    update dat hang

--- THTFood/app/src/main/java/com/example/thtfood/Controller/RestaurantActivity.java
+++ THTFood/app/src/main/java/com/example/thtfood/Controller/RestaurantActivity.java
@@ -2,6 +2,7 @@ package com.example.thtfood.Controller;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,12 +13,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.thtfood.Controller.ProductAdapter;
+import com.example.thtfood.Model.CartItem;
 import com.example.thtfood.Model.Product;
 import com.example.thtfood.R;
 import com.google.firebase.database.DataSnapshot;
@@ -38,16 +41,23 @@ public class RestaurantActivity extends AppCompatActivity {
     private ImageView imageRestarant;
     private String restaurantKey;
     private ArrayList<Product> productList;
+    private CartViewModel cartViewModel;
 
+    private ImageButton imageButtonQuit;
+    private TextView sizecart;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        cartViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance(getApplication())).get(CartViewModel.class);
         imageRestarant = findViewById(R.id.imageRestarant);
         nameTextView = findViewById(R.id.nameRes);
         addressTextView = findViewById(R.id.adressRes);
+        imageButtonQuit = findViewById(R.id.imageButtonQuit);
+        sizecart = findViewById(R.id.sizecart);
         Intent intent = getIntent();
         if (intent != null) {
             String name = intent.getStringExtra("name");
@@ -60,7 +70,24 @@ public class RestaurantActivity extends AppCompatActivity {
         }
         productList = new ArrayList<>();
 
+        imageButtonQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        if(cartViewModel.getResId() == null || !cartViewModel.getResId().equals(restaurantKey))
+        {
 
+            cartViewModel.SetResId(restaurantKey);
+            cartViewModel.clearCartItemsFromSharedPreferences(RestaurantActivity.this);
+            cartViewModel.clearCart();
+            cartViewModel.saveCartItemsToSharedPreferences(RestaurantActivity.this);
+
+        }
+        else{
+
+        }
         DatabaseReference restaurantRef = FirebaseDatabase.getInstance().getReference().child("restaurants").child(restaurantKey);
         // Lấy restaurant thông qua restaurantKey
         restaurantRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -73,6 +100,7 @@ public class RestaurantActivity extends AppCompatActivity {
                     int numItemsToShow = 0;
                     for (DataSnapshot childSnapshot : menuSnapshot.getChildren()) {
                         numItemsToShow += 1;
+                        String menuKey = childSnapshot.getKey();
                         // Lấy giá trị của thuộc tính "active"
                         Boolean isActive = childSnapshot.child("active").getValue(Boolean.class);
 
@@ -81,7 +109,7 @@ public class RestaurantActivity extends AppCompatActivity {
                             String descriptionmenu = childSnapshot.child("description").getValue(String.class);
                             String imagemenu = childSnapshot.child("image").getValue(String.class);
                             Double price = childSnapshot.child("price").getValue(Double.class);
-                            Product product = new Product(namemenu, imagemenu, price,descriptionmenu, true);
+                            Product product = new Product(menuKey, namemenu, imagemenu, price,descriptionmenu, true);
                             productList.add(product);
                         }
                     }
@@ -96,6 +124,8 @@ public class RestaurantActivity extends AppCompatActivity {
                         public void onProductClick(Product product) {
 
                             Intent intent = new Intent(RestaurantActivity.this, ProductDetailActivity.class);
+                            intent.putExtra("restaurantKey", restaurantKey);
+                            intent.putExtra("product_Key", product.getId());
                             intent.putExtra("product_name", product.getName());
                             intent.putExtra("product_price", product.getPrice());
                             startActivity(intent);
@@ -120,7 +150,29 @@ public class RestaurantActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Chuyển đến CartActivity khi người dùng nhấn vào nút xem giỏ hàng
                 Intent cartIntent = new Intent( RestaurantActivity.this, CartActivity.class);
+                cartIntent.putExtra("restaurantKey", restaurantKey);
                 startActivity(cartIntent);
+            }
+        });
+
+
+        cartViewModel.getCartItemsLiveData().observe(this, new Observer<List<CartItem>>() {
+            @Override
+            public void onChanged(List<CartItem> cartItems) {
+                sizecart.setText(String.valueOf(cartViewModel.getCartItemCount()));
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Reload or refresh your activity here
+        cartViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance(getApplication())).get(CartViewModel.class);
+        cartViewModel.getCartItemsLiveData().observe(this, new Observer<List<CartItem>>() {
+            @Override
+            public void onChanged(List<CartItem> cartItems) {
+                sizecart.setText(String.valueOf(cartViewModel.getCartItemCount()));
             }
         });
 
